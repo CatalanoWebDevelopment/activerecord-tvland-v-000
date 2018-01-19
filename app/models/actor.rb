1e25e2441633df74d385b167d29bd9f537591b8d
Parker Catalano <catalano.work@gmail.com>
2018-01-19 19:04:26 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,7 +6,9 @@ class Actor < ActiveRecord::Base
 
   def full_name
     binding.pry
-    first_name = self
+    first_name = self.first_name
+    last_name = self.last_name
+    puts "#{first_name} #{last_name}"
   end
 
 end
