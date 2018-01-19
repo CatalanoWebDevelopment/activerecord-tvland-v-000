765caba1e3456a9371c00793d9a10fd07f2f77f0
Parker Catalano <catalano.work@gmail.com>
2018-01-19 19:07:53 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -4,14 +4,14 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
-  def list_roles
-    binding.pry
-  end
-
   def full_name
     first_name = self.first_name
     last_name = self.last_name
     puts "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    binding.pry
   end
 
 end
